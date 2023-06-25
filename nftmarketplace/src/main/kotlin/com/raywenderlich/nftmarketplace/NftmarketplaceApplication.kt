442f978277a2ed77165b2a83fededee7a100e98f
VAShapovalova <17ametista17@gmail.com>
2023-06-25 14:00:15 +0700

    #1 add ntfmarketplace

---
+++ nftmarketplace/src/main/kotlin/com/raywenderlich/nftmarketplace/NftmarketplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.nftmarketplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NftmarketplaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NftmarketplaceApplication>(*args)
+}
---
+++ nftmarketplace/src/test/kotlin/com/raywenderlich/nftmarketplace/NftmarketplaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raywenderlich.nftmarketplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NftmarketplaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
